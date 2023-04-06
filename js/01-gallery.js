d612320631948d435271824b5c97d03eb44fe0b3
denis-marul <marul2na3@gmail.com>
2023-04-06 16:46:22 +0300

    HWseven
    HWseven

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+
+const galleryElements = galleryItems
+  .map(element => {
+    const { original, preview, description } = element;
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join('');
+
+galleryUl.insertAdjacentHTML('afterbegin', galleryElements);
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryUl.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+galleryUl.addEventListener('click', openModal);
---
+++ js/02-lightbox.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+
+const galleryElements = galleryItems
+  .map(element => {
+    const { original, preview, description } = element;
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+galleryUl.insertAdjacentHTML('afterbegin', galleryElements);
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+  });
+}
+
+galleryUl.addEventListener('click', openModal);
