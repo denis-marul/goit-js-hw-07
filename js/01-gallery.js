226dc267723b3b3e54973e284a7841772a894799
denis-marul <marul2na3@gmail.com>
2023-04-07 15:28:56 +0300

    HWsvenNew
    HWsvenNew

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,17 +28,28 @@ function openModal(event) {
   if (event.target.nodeName !== 'IMG') {
     return;
   }
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-
-  instance.show();
-
-  galleryUl.addEventListener('keydown', event => {
+  function escape(event) {
     if (event.code === 'Escape') {
+      console.log('listen');
       instance.close();
     }
-  });
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`,
+    {
+      onShow: instance => {
+        galleryUl.addEventListener('keydown', escape);
+      },
+
+      onClose: instance => {
+        galleryUl.removeEventListener('keydown', escape);
+      },
+    }
+  );
+
+  instance.show();
 }
 
 galleryUl.addEventListener('click', openModal);
